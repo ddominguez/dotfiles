decc219168a5b27c9a03508e6886a47b01e2833f
David Dominguez <352985+ddominguez@users.noreply.github.com>
2022-12-17 15:20:31 -0500

    Update telescope keymaps

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,9 @@
-local opts = { noremap=true, silent=true }                                                                                                                   
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>', opts)
+local builtin = require("telescope.builtin")
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 --[[local telescope = require'telescope'
 telescope.setup({
