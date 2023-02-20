b1fd53c97be6d7b4d1c8c6665f21db913254f773
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-02-19 21:54:27 -0500

    Update lualine

--- neovim/.config/nvim/after/plugin/lualine.lua
+++ neovim/.config/nvim/after/plugin/lualine.lua
@@ -1,5 +1,11 @@
-require("lualine").setup{
+require("lualine").setup {
     options = {
         icons_enabled = false,
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+    },
+    sections = {
+        lualine_b = { "branch" },
+        lualine_c = { "diff", "diagnostics", { "filename", path = 1 } }
     }
 }
