03fd403d002b84a9912e289562c94abb73aabaf7
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-05-19 20:40:30 -0400

    added debugger tools

---
+++ neovim/.config/nvim/after/plugin/dap.lua
@@ -0,0 +1,27 @@
+local dap, dapui = require("dap"), require("dapui")
+
+vim.keymap.set('n', '<F5>', function() dap.continue() end)
+vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() dap.repl.toggle() end)
+vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+
+local pyenv_dir = os.getenv("PYVENV_DIR")
+require("dap-python").setup(pyenv_dir .. "/debugpy/bin/python")
+require("dap-go").setup()
+require("dapui").setup()
+require("nvim-dap-virtual-text").setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
