4685f5ad42745f5997f1d4638f971bcc1de94553
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-06-24 16:41:28 -0400

    editor updates

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,13 +6,13 @@ set.softtabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
 set.smartindent = true
-set.number = true
-set.relativenumber = true
+-- set.number = true
+-- set.relativenumber = true
 set.hlsearch = false
 set.incsearch = true
 set.wrap = false
 
-set.scrolloff = 8
+set.scrolloff = 6
 set.cmdheight = 1
 set.colorcolumn = "80"
 set.signcolumn = "yes"
@@ -26,4 +26,3 @@ vim.g.mapleader = " "
 
 require "dave.plugins"
 require "dave.theme"
-
