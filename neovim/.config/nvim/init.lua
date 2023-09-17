80e1ec32719cd66b7370ecc240f8d3493756216c
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-09-17 15:41:57 -0400

    set indent size to 2 for htmldjango

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -30,7 +30,10 @@ set.laststatus = 1
 vim.api.nvim_create_augroup('setIndent', { clear = true })
 vim.api.nvim_create_autocmd('Filetype', {
     group = 'setIndent',
-    pattern = { 'css', 'html', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json', 'jsonc' },
+    pattern = {
+        'css', 'html', 'htmldjango', 'javascript', 'javascriptreact',
+        'typescript', 'typescriptreact', 'json', 'jsonc'
+    },
     command = 'setlocal shiftwidth=2 tabstop=2 softtabstop=2'
 })
 
