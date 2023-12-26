76d0f255bddbc8a6b735d57fd6206626426cafa1
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-12-25 21:43:53 -0500

    update telescope and remove tokyonight

--- neovim/.config/nvim/lua/dave/plugins.lua
+++ neovim/.config/nvim/lua/dave/plugins.lua
@@ -1,12 +1,11 @@
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
     use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.3",
+        "nvim-telescope/telescope.nvim", branch = "0.1.x",
         requires = {
             { "nvim-lua/plenary.nvim" }
         }
     }
-    use("folke/tokyonight.nvim")
     use {
         "VonHeikemen/lsp-zero.nvim",
         requires = {
