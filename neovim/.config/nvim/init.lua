54a1f72e3a72e96099b57670b434673982ec40c3
David Dominguez <352985+ddominguez@users.noreply.github.com>
2025-03-06 16:16:23 -0500

    enable undercurls

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -68,6 +68,18 @@ vim.api.nvim_set_hl(0, 'Pmenu', { ctermbg = 0 })
 vim.api.nvim_set_hl(0, 'Statement', { ctermfg = 3, bold = false })
 vim.api.nvim_set_hl(0, 'Conceal', { ctermfg = 7 })
 vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 0 })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", vim.tbl_extend("force",
+    vim.api.nvim_get_hl(0, { name = "DiagnosticUnderlineError" }),
+    { cterm = { undercurl = true }, undercurl = true }
+))
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", vim.tbl_extend("force",
+    vim.api.nvim_get_hl(0, { name = "DiagnosticUnderlineWarn" }),
+    { cterm = { undercurl = true }, undercurl = true }
+))
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", vim.tbl_extend("force",
+    vim.api.nvim_get_hl(0, { name = "DiagnosticUnderlineInfo" }),
+    { cterm = { undercurl = true }, undercurl = true }
+))
 
 -- go templ ft
 vim.filetype.add({
