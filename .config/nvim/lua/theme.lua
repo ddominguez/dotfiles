ef14557db706d1aafa99c9b27a46503b460db598
David Dominguez <352985+ddominguez@users.noreply.github.com>
2026-05-02 12:07:24 -0400

    Update colors and themes

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,4 +1,7 @@
 local function is_kitty_light_theme()
+    if vim.env.TERM ~= "xterm-kitty" then
+        return false
+    end
     local theme_file = vim.fn.expand("~/.config/kitty/theme.conf")
     local handle = io.open(theme_file, "r")
     if not handle then return false end
@@ -7,7 +10,7 @@ local function is_kitty_light_theme()
     return first_line and first_line:find("light theme") ~= nil
 end
 
-local is_light = is_kitty_light_theme()
+local is_light_theme = is_kitty_light_theme()
 
 vim.opt.termguicolors = false
 vim.cmd("colorscheme default")
@@ -23,24 +26,30 @@ vim.api.nvim_set_hl(0, "StatusLineNC", { cterm = { underline = true } })
 -- and should it also have the undercurl??
 local DiagnosticLevels = { "Error", "Warn", "Info", "Hint" }
 for _, level in pairs(DiagnosticLevels) do
-    vim.api.nvim_set_hl(0, "DiagnosticUnderline" .. level, vim.tbl_extend("force",
-        vim.api.nvim_get_hl(0, { name = "DiagnosticUnderline" .. level }),
-        { cterm = { undercurl = true }, undercurl = true }
-    ))
+    vim.api.nvim_set_hl(0, "DiagnosticUnderline" .. level, {
+        update = true, undercurl = true
+    })
 end
 
-if is_light then
+-- TODO: Try getting specific colors from kitty
+-- kitten @ get-colors | grep "^color1 " | tr -s " " | cut -d" " -f2
+
+if is_light_theme then
     vim.api.nvim_set_hl(0, "Normal", { ctermfg = 0 })
     vim.api.nvim_set_hl(0, "Comment", { ctermfg = 15 })
-    vim.api.nvim_set_hl(0, "FloatBorder", { ctermfg = 12 })
+    vim.api.nvim_set_hl(0, "Pmenu", { reverse = true })
     vim.api.nvim_set_hl(0, "PmenuThumb", { ctermbg = 12 })
     vim.api.nvim_set_hl(0, "Statement", { ctermfg = 5, bold = true })
     vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 103 })
     vim.api.nvim_set_hl(0, "String", { ctermfg = 4 })
-    vim.api.nvim_set_hl(0, "Function", { ctermfg = 8 })
+    vim.api.nvim_set_hl(0, "Special", { ctermfg = 0 })
+    vim.api.nvim_set_hl(0, "Function", { ctermfg = 0 })
+    vim.api.nvim_set_hl(0, "Operator", { ctermfg = 5, bold=true })
     vim.api.nvim_set_hl(0, "Search", { ctermfg = 0, ctermbg = 10 })
     vim.api.nvim_set_hl(0, "CurSearch", { ctermfg = 0, ctermbg = 10 })
     vim.api.nvim_set_hl(0, "Error", { ctermfg = 1, bold = true })
+    vim.api.nvim_set_hl(0, "StatusLine", { ctermbg = 103, ctermfg = 0, bold = true })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { ctermbg = 103, ctermfg = 8 })
 else
     vim.api.nvim_set_hl(0, "Normal", { ctermfg = 7 })
     vim.api.nvim_set_hl(0, "Comment", { ctermfg = 8 })
@@ -49,6 +58,7 @@ else
     vim.api.nvim_set_hl(0, "PmenuThumb", { ctermbg = 12 })
     vim.api.nvim_set_hl(0, "Statement", { ctermfg = 5, bold = false })
     vim.api.nvim_set_hl(0, "PreProc", { bold = false })
-    vim.api.nvim_set_hl(0, "Conceal", { ctermfg = 7 })
     vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 0 })
+    vim.api.nvim_set_hl(0, "StatusLine", { ctermbg = 0, ctermfg = 7, bold = true })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { ctermbg = 0, ctermfg = 8 })
 end
